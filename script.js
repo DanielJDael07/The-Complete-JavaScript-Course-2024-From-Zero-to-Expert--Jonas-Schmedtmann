6465b5fb6912ec38a70f62872ce331f3c98831a2
Daniel Dael <danieldael7777@gmail.com>
2024-05-29 23:42:58 +0800

    Introduction to Array

--- script.js
+++ script.js
@@ -37,9 +37,9 @@
 
 // Function Declaration
 
-function calcAge1(birthYear) {
-  return   2037 - birthYear;
-}
+// function calcAge1(birthYear) {
+//   return   2037 - birthYear;
+// }
 
 // const myAge1 = calcAge1(1992);
 // console.log(`My age in Year 2037 is ${myAge1}.`);
@@ -58,27 +58,74 @@ function calcAge1(birthYear) {
 
 
 // Function Expression
-const calcAge2 = function (birthYear) {
-    return 2037 - birthYear;
-}
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
 
 
 // Arrow Function
 
-const calcAge3 = birthYear => 2037 - birthYear;
+// const calcAge3 = birthYear => 2037 - birthYear;
 
-const myAge3 = calcAge3(1992);
+// const myAge3 = calcAge3(1992);
 
-console.log(`My age in Year 20237 is ${myAge3}.`);
+// console.log(`My age in Year 20237 is ${myAge3}.`);
 
-const yearsUntilRetirement = (birthYear,firstname) => {
-    const age = 2037 - birthYear;
-    const retirement = 65 - age;
-    // return retirement;
-    return `${firstname} retires in ${retirement} years.`;
-}
+// const yearsUntilRetirement = (birthYear,firstname) => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     // return retirement;
+//     return `${firstname} retires in ${retirement} years.`;
+// }
 
 
 
-console.log(yearsUntilRetirement(1992 , 'Daniel'))
-console.log(yearsUntilRetirement(1996 , 'Gelie'))
+// console.log(yearsUntilRetirement(1992 , 'Daniel'))
+// console.log(yearsUntilRetirement(1996 , 'Gelie'))
+
+
+//Functions Calling other Functions
+
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+
+
+// function fruitProcessor(apples, oranges) {
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
+
+//     const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} piece of orange.`;
+//     return juice;
+// }
+
+// console.log(fruitProcessor(2,3))
+
+// const calcAge =function (birthYear) {
+//   return 2037 - birthYear;
+// } 
+
+
+
+
+// const yearsUntilRetirement = function (birthYear,firstname) {
+//     const age = calcAge(birthYear);
+//     const retirement = 65 - age;
+//       if(retirement > 0) {
+//         return retirement
+//       } else {
+//         return -1;
+//       }
+//       // return `${firstname} retires in ${retirement} years.`;
+      
+// }
+
+// console.log(yearsUntilRetirement (1992,'Daniel'));
+// console.log(yearsUntilRetirement (1950,'John'));
+
+
+const friends = ['Daniel','Rupert','Gelie','John Lloyd']; //-Literal syntax, usual way of using array
+
+console.log(friends);
+
+const years = new Array(1991,1992,1993,1994,2000,2005); // another way of creating array

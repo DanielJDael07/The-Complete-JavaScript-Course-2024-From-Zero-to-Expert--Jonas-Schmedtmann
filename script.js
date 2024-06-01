66f4687de8a2889536db3e95a37ea59d23983e53
Daniel Dael <danieldael7777@gmail.com>
2024-06-01 22:58:21 +0800

    JS object

--- script.js
+++ script.js
@@ -124,8 +124,136 @@
 // console.log(yearsUntilRetirement (1950,'John'));
 
 
-const friends = ['Daniel','Rupert','Gelie','John Lloyd']; //-Literal syntax, usual way of using array
+// const friends = ['Daniel','Rupert','Gelie','John Lloyd']; //-Literal syntax, usual way of using array
 
-console.log(friends);
+// console.log(friends);
 
-const years = new Array(1991,1992,1993,1994,2000,2005); // another way of creating array
+// const years = new Array(1991,1992,1993,1994,2000,2005); // another way of creating array.
+
+// console.log(friends[0]);
+// console.log(friends[2]);
+
+// console.log(friends.length); // to count the values in array.
+// console.log(friends[friends.length - 1]); // to get the last value of array -1, -2...if you want to get 2nd to the last etc. 
+
+// friends[1] = 'Jay'; // to change array value.
+
+// console.log(friends);
+
+// const firstname = 'Daniel';
+
+// const daniel = [firstname,'Dael', 2024 - 1992, 'Web Developer',friends];
+
+
+// console.log(daniel);
+
+// // Exercise
+
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const years2 = [1991,1992,1993,1994,2000,2005];
+
+// const age1 = calcAge2(years2[1]);
+// const age2 = calcAge2(years2[2]);
+// const age3 = calcAge2(years2[years2.length -1]);
+
+// console.log (age1,age2,age3);
+
+// const ages = [calcAge2(years2[1]) ,calcAge2(years2[2]), calcAge2(years2[years2.length -1])] ;
+
+// console.log (ages);
+
+
+// Add Elements
+
+// const friends = ['Daniel','Rupert','Gelie','John Lloyd']; 
+
+// const newLength = friends.push('Jay');
+
+
+// console.log(newLength);
+
+// friends.unshift('John');
+
+// console.log(friends);
+
+// // Remove Elements
+
+// friends.pop(); // remove the last element of array
+
+// const popped = friends.pop();
+
+// console.log(popped);
+
+// console.log(friends);
+
+// friends.shift();
+// console.log(friends);
+
+
+
+// console.log(friends.indexOf('Gelie'));
+
+
+// console.log(friends.includes('Dani')) // to determine if the element is included in Array
+
+
+
+// Objects
+
+// Object literal syntax
+const daniel = {
+    firstName: 'Daniel',
+    lastName: 'Dael',
+    age: 2037 - 1992,
+    job: 'Web Developer',
+    friends: ['Gelie','Rupert', 'Yendell', 'John Lloyd']
+};
+
+console.log(daniel);
+
+// Dot notation - use to retrieve data from objects
+console.log(daniel.lastName);
+
+// Bracket notation - another way of retrieving data from objects.We can put expression in this notation.
+console.log(daniel['lastName']);
+
+const nameKey ='Name';
+console.log(daniel['first' + nameKey]);
+console.log(daniel['last'+ nameKey]);
+
+const interestedIn = prompt('What do you want to know about Daniel? Choose between firstName, lastName, age, job, and friends');
+// console.log(daniel[interestedIn]);
+
+
+
+if (daniel[interestedIn]) {
+    console.log(daniel[interestedIn]);    
+} else {
+    console.log('Wrong request, Choose between firstName, lastName, age, job, and friends ')
+};
+
+daniel.location = 'Philippines';
+daniel['facebook'] = 'Daniel Josue Dael';
+console.log(daniel);
+
+
+// Challenge
+// daniel has 4 friends and his bestfriend is Gelie.
+
+console.log(`${daniel.firstName} has ${daniel.friends.length} friends and his bestfriend is ${daniel.friends[0]}.`)
+
+
+const daniel = {
+    firstName: 'Daniel',
+    lastName: 'Dael',
+    age: 2037 - 1992,
+    job: 'Web Developer',
+    friends: ['Gelie','Rupert', 'Yendell'],
+    hasDriversLicense: true,
+    calcAge: function(birthYear) {    // Any function that is attached to Object is called method.
+        return 2037 - birthYear
+    }
+};
